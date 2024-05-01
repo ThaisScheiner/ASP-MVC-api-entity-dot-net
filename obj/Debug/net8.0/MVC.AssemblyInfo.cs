1de707c0663a3c4642be19316c17246b8a784a8d
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2024-05-01 01:20:38 -0300

    update index home

--- obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179b348e5a77974d8de9591293332a46e682db80")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
